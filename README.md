a653599b938624d96cd0b76e6a7af6e1e8db78f5
Simon Mirco <simon.mirco@rackspace.com>
2015-02-03 10:37:09 +1100

    Done proof of concept

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ Currently lsync config files need to be generated on the master and need to cont
 If slave servers come and go (such as in an autoscale environment) these need to be managed automatically in the lsyncd config file.
 
 This project aims to read autoscale group members from API calls and generate an lsyncd configuration file from this information.
+
+
+Setup
+=====
+
+Create a file called .cloud_credentials with contents like the following
+
+```
+[rackspace_cloud]
+username = yourusernamehere
+api_key = yourapikeyhere
+```
+
+By default this script will have source and destination directories set to /var/www
+modify main.py to change this
+
+TODO: config files ;)
+
+pyrax throws some warning output to stderr so to execute this script use the following command line
+
+
+```
+python main.py --as-group=AUTOSCALE_GROUP_ID 2>/dev/null > lsyncd.conf
+```
+
+This can be added to a cronjob
+
---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+import pyrax
+import argparse
+import os
+from auth import Auth
+import jinja2
+from jinja2 import Template
+from jinja2 import Environment, PackageLoader
+
+def generate_lsync_config(addresses):
+  env = Environment(autoescape=True, 
+                    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+  template = env.get_template('lsyncd.conf')
+
+  print template.render(server_list=addresses, source_dir='/var/www', dest_dir='/var/www')
+     
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--as-group', required=True,
+                      help='The autoscale group config ID')
+
+  args = vars(parser.parse_args())
+
+  pyrax.set_setting("identity_type", "rackspace")
+
+  # Using credentials file
+  try:
+    pyrax.set_credential_file("./.cloud_credentials")
+  except Exception, e:
+    sys.stderr.write("Failed to authenticate: %s" % str(e))
+
+  au = pyrax.autoscale
+  cs = pyrax.cloudservers
+
+  as_group = au.get(args['as_group']);
+
+  snet_ips = []
+
+  for s_id in as_group.get_state()['active']:
+    server = cs.servers.get(s_id)
+    snet_ips.append(server.networks['private'][0])
+
+  generate_lsync_config(snet_ips)
+
+if __name__ == '__main__':
+  main()
