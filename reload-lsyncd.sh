0d1c5619dc977bc491714f9a8dc415b29008aa0f
Simon Mirco <simon.mirco@rackspace.com>
2015-02-03 15:35:29 +1100

    Script to determine if reload is required

---
+++ reload-lsyncd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LAST_HASH=$(< ~/.lsyncd_config_hash)
+CURRENT_HASH=$(md5sum /etc/lsyncd.conf | awk '{print $1}')
+
+echo "Current Hash $CURRENT_HASH"
+echo "Last Hash $LAST_HASH"
+
+if [[ "$LAST_HASH" != "$CURRENT_HASH" ]]; then
+  echo 'lsync config hash changed'
+  /sbin/service lsyncd reload
+  echo $CURRENT_HASH > ~/.lsyncd_config_hash
+else
+  echo 'lsync config not updated'
+fi
